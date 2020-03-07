4187ac1d2295fa042f36e59eafa5437be64b097a
Fadhli Al Mutawakkil <fadhli.tech@gmail.com>
2020-03-07 15:20:32 +0700

    Selesai Login

--- app/src/main/java/com/example/sekolahku/MainActivity.kt
+++ app/src/main/java/com/example/sekolahku/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.sekolahku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var email = ""
+    var pass = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            email = edt_email.text.toString()
+            pass = edt_pass.text.toString()
+
+            checkLogin(email, pass)
+        }
     }
+
+    fun checkLogin(email: String, pass: String) {
+        if (email.equals("akil") && pass.equals("123")) {
+            showToast("Login Berhasil")
+        } else {
+            showToast("Login gagal")
+        }
+    }
+
+    fun showToast(msg: String) {
+        Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT).show()
+    }
+
 }
---
+++ app/src/main/java/com/example/sekolahku/training/Training.kt
@@ -0,0 +1,25 @@
+package com.example.sekolahku.training
+
+import com.example.sekolahku.R
+
+class Training {
+
+    //variable mutable
+    var nama: String = "sdsff"
+    var umur: Int = 3
+    var mahasiswa: Boolean = true
+    var tinggi: Double = 3.0
+
+    var nama1 = "sdsff"
+    var umur1 = 3
+    var mahasiswa1 = true
+    var tinggi1 = 3.0
+
+    //variable immutable
+    val sekolah = "Tidak"
+
+    fun main(arg: Array<String>) {
+
+    }
+
+}
